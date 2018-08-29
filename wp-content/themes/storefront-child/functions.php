df504d95af75b27257e0bcf3c69688d1b9954671
Jonathan Siderman <jsiderman@mydevices.com>
2018-08-28 18:00:31 -0700

    Dequeue storefront styles in child theme

---
+++ wp-content/themes/storefront-child/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Functions and cusotmizations for the DGTL theme
+ */
+
+/**
+ * Dequeue the Storefront Parent theme core CSS
+ */
+function sf_child_theme_dequeue_style() {
+	wp_dequeue_style( 'storefront-style' );
+	wp_dequeue_style( 'storefront-woocommerce-style' );
+}
+
